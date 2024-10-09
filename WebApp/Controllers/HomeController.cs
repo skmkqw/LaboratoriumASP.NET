fa591fa72fd3eb88a681a9f3d4d33b7f5c02999f
skmkqw <tkorsakov77@gmail.com>
2024-10-09 11:44:38 +0200

    calculator

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -25,6 +25,35 @@ public class HomeController : Controller
     
     public IActionResult Calculator()
     {
+        var op = Request.Query["op"];
+        var x = double.Parse(Request.Query["x"]);
+        var y = double.Parse(Request.Query["y"]);
+        double result = 0.0;
+
+        switch (op)
+        {
+            case "add":
+                result = x + y;
+                ViewBag.Operation = "+";
+                break;
+            case "sub":
+                result = x - y;
+                ViewBag.Operation = "-";
+                break;
+            case "mul":
+                result = x * y;
+                ViewBag.Operation = "*";
+                break;
+            case "div":
+                result = x / y;
+                ViewBag.Operation = "/";
+                break;
+        }
+        
+        ViewBag.Result = result;
+        ViewBag.X = x;
+        ViewBag.Y = y;
+        
         return View();
     }
 
