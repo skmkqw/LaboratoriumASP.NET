8d3d046e4f0f6a8449b00824c5f428c738826472
skmkqw <tkorsakov77@gmail.com>
2024-10-16 11:30:14 +0200

    calculator using form

---
+++ WebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class CalculatorController : Controller
+{
+    public IActionResult Form()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Result([FromForm] Calculator model)
+    {
+        if (!model.IsValid())
+        {
+            return View("Error");
+        }
+        return View(model);
+    }
+}
