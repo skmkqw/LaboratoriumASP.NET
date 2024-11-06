79d64eb7730b521277f7c372201401d75738eb6f
skmkqw <tkorsakov77@gmail.com>
2024-11-06 11:41:10 +0100

    created initial migration

---
+++ WebApp/Migrations/20241106104004_Initial.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    phone = table.Column<string>(type: "TEXT", nullable: false),
+                    BirthDate = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Category = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "BirthDate", "Category", "CreatedAt", "Email", "FirstName", "LastName", "phone" },
+                values: new object[,]
+                {
+                    { 1, new DateOnly(2000, 10, 10), 3, new DateTime(2024, 11, 6, 11, 40, 3, 219, DateTimeKind.Local).AddTicks(7846), "johndoe@gmail.com", "John", "Doe", "123123123" },
+                    { 2, new DateOnly(2002, 1, 23), 2, new DateTime(2024, 11, 6, 11, 40, 3, 219, DateTimeKind.Local).AddTicks(7895), "janedoe@gmail.com", "Jane", "Doe", "321231321" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
