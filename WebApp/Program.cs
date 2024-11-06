41c7b15d98190c210416caa33254a341645e53f9
skmkqw <tkorsakov77@gmail.com>
2024-11-06 11:37:45 +0100

    registered EfContactService and AppDbContext in DI container

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,3 +1,4 @@
+using WebApp.Models;
 using WebApp.Models.Services;
 
 namespace WebApp;
@@ -8,18 +9,17 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddControllersWithViews();
 
-        builder.Services.AddSingleton<IContactService, ContactService>();
+        builder.Services.AddDbContext<AppDbContext>();
+
+        builder.Services.AddScoped<IContactService, EfContactService>();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
