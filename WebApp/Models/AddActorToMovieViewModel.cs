fc3a476c2de388b1379a112585b0a766ccd844d3
skmkqw <tkorsakov77@gmail.com>
2024-12-23 17:59:55 +0100

    form validation

--- WebApp/Models/AddActorToMovieViewModel.cs
+++ WebApp/Models/AddActorToMovieViewModel.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApp.Models;
 
 public class AddMovieToActorViewModel
 {
     public int ActorId { get; set; }
-
+    
     public string ActorFullName { get; set; }
+    
+    [Required(ErrorMessage = "Please select a movie")]
     public int MovieId { get; set; }
+    
+    [Required(ErrorMessage = "Please enter a character name")]
+    [MaxLength(200, ErrorMessage = "Character name cannot be longer than 200 characters")]
     public string CharacterName { get; set; }
     public List<MovieModel> AvailableMovies { get; set; }
 }
